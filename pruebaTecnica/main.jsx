d5b20376094e7bda78b14571357b0f65545a822f
Cristian Londoño Franco <69688343+CristianLF0903@users.noreply.github.com>
2024-08-15 16:57:08 -0500

    init pruebaTecnica

---
+++ pruebaTecnica/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './src/App.jsx'
+import './style.css'
+
+createRoot(document.getElementById('app').render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+))
---
+++ pruebaTecnica/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <main>
+            <h1>Buscador de películas</h1>
+        </main>
+    )
+}
